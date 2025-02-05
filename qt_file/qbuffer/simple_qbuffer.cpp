568e129784e3c2f9ba1cd3ba63765a3d6c8e4477
Dzher <2539925009@qq.com>
2025-02-05 11:29:35 +0800

    add qbuffer using demo

---
+++ qt_file/qbuffer/simple_qbuffer.cpp
@@ -0,0 +1,75 @@
+#include <qapplication.h>
+#include <qbuffer.h>
+#include <qdebug.h>
+#include <qglobal.h>
+#include <qobject.h>
+#include <qpalette.h>
+#include <QBuffer>
+#include <QByteArray>
+#include <QDataStream>
+#include <QDebug>
+#include <QPalette>
+
+void readWriteBuffer() {
+    QBuffer buffer;
+    buffer.open(QIODevice::ReadWrite);
+
+    buffer.write("hello qbuffer!");
+    buffer.seek(0);  // attention here, must do this step!
+    QByteArray data = buffer.readAll();
+    buffer.close();
+
+    qDebug() << "The buffer data is" << data;
+}
+
+void serialization() {
+    QBuffer buffer;
+    buffer.open(QIODevice::ReadWrite);
+
+    QDataStream write(&buffer);
+    write << QString("First") << int(2);
+    buffer.seek(0);
+
+    QDataStream read(&buffer);
+    QString first;
+    int second;
+    read >> first >> second;
+
+    buffer.close();
+
+    qDebug() << "First data is" << first << "second data is" << second;
+}
+
+void writeToByteArray(QByteArray& data) {
+    // QByteArray data;
+    QBuffer buffer(&data);
+    buffer.open(QIODevice::WriteOnly);
+
+    QDataStream write(&buffer);
+    write << QApplication::palette();
+    buffer.close();
+
+    qDebug() << "The read data is" << data;
+}
+
+void readFromByteArray(QByteArray& data) {
+    // QByteArray data;
+    QBuffer buffer(&data);
+    buffer.open(QIODevice::ReadOnly);
+
+    QPalette palette;
+    QDataStream read(&buffer);
+    read >> palette;
+
+    buffer.close();
+}
+
+int main() {
+    readWriteBuffer();
+    serialization();
+
+    QByteArray data;
+    writeToByteArray(data);
+    readFromByteArray(data);
+    return 0;
+}
